644eaabfdc59497c1034b8f3d7d0b748274e7fa6
Kevin <kevin.kds80@gmail.com>
2024-12-22 10:21:04 -0300

    format code

--- dot-inspections/components/SortButton.tsx
+++ dot-inspections/components/SortButton.tsx
@@ -1,27 +1,31 @@
 interface SortButtonProps {
-    path: string
-    fieldName: string,
-    sortStyle: string,
-    sortField: string
+  path: string;
+  fieldName: string;
+  sortStyle: string;
+  sortField: string;
 }
 
 export function SortButton(props: SortButtonProps) {
   const sortStyle = props.sortStyle ?? "desc";
   const options: Record<string, string> = {
     desc: "asc",
-    asc: "desc"
-  }
+    asc: "desc",
+  };
 
   const arrows: Record<string, string> = {
     desc: "⬇️",
-    asc: "⬆️"
-  }
+    asc: "⬆️",
+  };
 
   return (
     <div class="flex flex-col">
-        <a href={`/${props.path}?field=${props.fieldName}&sort=${options[sortStyle]}`}>
-            {props.sortField == props.fieldName ? arrows[sortStyle] : "🟦"}
-        </a>
+      <a
+        href={`/${props.path}?field=${props.fieldName}&sort=${
+          options[sortStyle]
+        }`}
+      >
+        {props.sortField == props.fieldName ? arrows[sortStyle] : "🟦"}
+      </a>
     </div>
   );
 }
--- dot-inspections/routes/inspections/index.tsx
+++ dot-inspections/routes/inspections/index.tsx
@@ -1,9 +1,11 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { InspectionsFilter } from "../../components/InpectionsFilter.tsx";
 import { InspectionsTable } from "../../components/Table.tsx";
 
 interface ListProps {
-  sort: string
-  field: string
+  sort: string;
+  field: string;
+  filter: string;
 }
 
 export const handler: Handlers<ListProps> = {
@@ -12,8 +14,8 @@ export const handler: Handlers<ListProps> = {
     const sort = url.searchParams.get("sort") || "asc";
     const field = url.searchParams.get("field") || "date";
     const filter = url.searchParams.get("filter") || "";
-    console.log(filter)
-    return ctx.render({ sort, field });
+    console.log(filter);
+    return ctx.render({ sort, field, filter });
   },
 };
 
@@ -24,43 +26,35 @@ export default function DotInspections(props: PageProps<ListProps>) {
     inspectionNumber: "inspectionNumber",
     vehiclePlate: "vehiclePlate",
     basic: undefined,
-    weight: undefined
-  }, 
-  {
+    weight: undefined,
+  }, {
     date: "adate",
     status: "astatus",
     inspectionNumber: "ainspectionNumber",
     vehiclePlate: "avehiclePlate",
     basic: undefined,
-    weight: undefined
-  }]
+    weight: undefined,
+  }];
 
   return (
     <body class="bg-slate-200">
       <div class="px-4 py-8 mx-auto">
-      <h1 class="text-3xl font-sans">Dot Inspections</h1>
-      <div class="rounded-xl p-8 bg-slate-300">
-        <form>
-          <div class="sm:col-span-3 pb-2">
-            <label for="filter" class="block text-sm/6 font-medium text-gray-900">BASIC</label>
-            <div class="mt-2 grid grid-cols-5">
-              <select name="filter" id="filter" class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                <option selected></option>
-                <option>option1</option>
-              </select>
-              <div></div>
-              <button class="sm:col-span-1 rounded-md bg-slate-600 text-sm/6 font-semibold text-gray-100" type="submit">Search</button>
-            </div>
-
-          </div>
-          <input name="sort" type="text" hidden value={props.data.sort} />
-          <input name="field" type="text" hidden value={props.data.field} />
-        </form>
-
-        <InspectionsTable sortField={props.data.field} sortStyle={props.data.sort} data={data}></InspectionsTable>
+        <h1 class="text-3xl font-sans">Dot Inspections</h1>
+        <div class="rounded-xl p-8 bg-slate-300">
+          <InspectionsFilter
+            filter={props.data.filter}
+            field={props.data.field}
+            sort={props.data.sort}
+          >
+          </InspectionsFilter>
+          <InspectionsTable
+            sortField={props.data.field}
+            sortStyle={props.data.sort}
+            data={data}
+          >
+          </InspectionsTable>
+        </div>
       </div>
-    </div>
     </body>
-    
   );
 }
