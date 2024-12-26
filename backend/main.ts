43578e345b8a9b2579d06220bc023f53901f91bd
Kevin <kevin.kds80@gmail.com>
2024-12-26 16:12:43 -0300

    refactor a bit

--- backend/main.ts
+++ backend/main.ts
@@ -3,20 +3,21 @@ import { fetchInspections, fetchInspectionDetails } from "./controllers/inspecti
 
 const router = new Router();
 
-router.get("/inspections/", (ctx) => {
-  const sort = ctx.request.url.searchParams.get("sort") ?? "";
-  const field = ctx.request.url.searchParams.get("field") ?? "";
-  const filter = ctx.request.url.searchParams.get("filter") ?? "";
-  const page = ctx.request.url.searchParams.get("page") ?? "1";
-  const limit = ctx.request.url.searchParams.get("filter") ?? "10";
-  fetchInspections(parseInt(page), parseInt(limit), sort, field, filter)
+router.get("/inspections/", async (ctx) => {
+  const url = ctx.request.url
+  const sort = url.searchParams.get("sort") ?? "";
+  const field = url.searchParams.get("field") ?? "";
+  const filter = url.searchParams.get("filter") ?? "";
+  const page = url.searchParams.get("page") ?? "1";
+  const limit = url.searchParams.get("filter") ?? "10";
+  await fetchInspections(parseInt(page), parseInt(limit), sort, field, filter)
   console.log(ctx.request)
   ctx.response.headers.append('Content-Type', 'application/json')
   ctx.response.body = { hi: "Hello world" };
 });
 
-router.get("/inspections/:inspection_number", (ctx) => {
-  fetchInspectionDetails(ctx.params.inspection_number)
+router.get("/inspections/:inspection_number", async (ctx) => {
+  await fetchInspectionDetails(ctx.params.inspection_number)
   ctx.response.headers.append('Content-Type', 'application/json')
   ctx.response.body = { hi: "Hello world" };
 });
