63d5cb4e9e37aa772e8e76981b6b2bfb7f1c46fa
Kevin <kevin.kds80@gmail.com>
2024-12-28 11:20:21 -0300

    change from functions to class

--- backend/controllers/inspections.ts
+++ backend/controllers/inspections.ts
@@ -1,54 +1,41 @@
+import { Database } from "@db/mongo";
 import { ResponseData } from "../dtos/response.ts";
 import { InspectionsSelector } from "../dtos/selectors.ts";
-import {
-  getInspectionDetails,
-  getInspections,
-} from "../services/inspections.ts";
+import { InspectionsService } from "../services/inspections.ts";
+import { validFilters } from "../validations/filters.ts";
+import { validSorts } from "../validations/sorts.ts";
+import { validFields } from "../validations/fields.ts";
 
-const validFilters: Record<string, string> = {
-  "Vehicle Maint.": "Vehicle Maint.",
-  "HOS Compliance": "HOS Compliance",
-  "Unsafe Driving": "Unsafe Driving",
-  "Driver Fitness": "Driver Fitness",
-  "Drugs/Alcohol": "Drugs/Alcohol",
-  "HM Compliance": "HM Compliance",
-};
+export class InspectionsController {
+  private db: Database;
+  private service: InspectionsService;
+  private MAX_LIMIT = 100;
+  private MIN_PAGE = 1;
 
-const validFields: Record<string, string> = {
-  date: "date",
-  status: "status",
-  inspectionNumber: "report_number",
-  vehiclePlate: "license_plate",
-  basic: "basic",
-  weight: "weight",
-};
+  constructor(db: Database) {
+    this.db = db;
+    this.service = new InspectionsService(this.db);
+  }
 
-const validSorts: Record<string, string> = {
-  asc: "asc",
-  desc: "desc",
-};
+  public async fetchInspections(
+    page: number,
+    limit: number,
+    sort: string,
+    field: string,
+    filter: string,
+  ): Promise<ResponseData> {
+    const selector: InspectionsSelector = {
+      page: page < this.MIN_PAGE ? this.MIN_PAGE : page,
+      limit: limit > this.MAX_LIMIT ? this.MAX_LIMIT : limit,
+      sort: validSorts[sort] || validSorts.desc,
+      filter: validFilters[filter],
+      field: validFields[field] || validFields.date,
+    };
 
-const MAX_LIMIT = 100;
-const MIN_PAGE = 1;
+    return await this.service.getInspections(selector);
+  }
 
-export async function fetchInspections(
-  page: number,
-  limit: number,
-  sort: string,
-  field: string,
-  filter: string,
-): Promise<ResponseData> {
-  const selector: InspectionsSelector = {
-    page: page < MIN_PAGE ? MIN_PAGE : page,
-    limit: limit > MAX_LIMIT ? MAX_LIMIT : limit,
-    sort: validSorts[sort] || validSorts.desc,
-    filter: validFilters[filter],
-    field: validFields[field] || validFields.date,
-  };
-
-  return await getInspections(selector);
-}
-
-export async function fetchInspectionDetails(inspectionNumber: string) {
-  return await getInspectionDetails(inspectionNumber);
+  public async fetchInspectionDetails(inspectionNumber: string) {
+    return await this.service.getInspectionDetails(inspectionNumber);
+  }
 }
--- backend/services/inspections.ts
+++ backend/services/inspections.ts
@@ -1,13 +1,26 @@
+import { Collection, Database } from "@db/mongo";
 import { ResponseData } from "../dtos/response.ts";
 import { InspectionsSelector } from "../dtos/selectors.ts";
+import { inspections } from "../repositories/inpections/client.ts";
+import { InspectionSchema } from "../repositories/inpections/schema.ts";
 
+export class InspectionsService {
+    private db: Database;
+    private inspections: Collection<InspectionSchema>;
 
-export async function getInspections(params: InspectionsSelector): Promise<ResponseData> {
-    await console.log(params)
-    return { code: 201, data: { hi: "Hello world from getInspections" } }
+    constructor(db: Database) {
+        this.db = db;
+        this.inspections = inspections(this.db);
+    }
+
+    public async getInspections(params: InspectionsSelector): Promise<ResponseData> {
+        await console.log(params)
+        return { code: 201, data: { hi: "Hello world from getInspections" } }
+    }
+    
+    public async getInspectionDetails(inspectionNumber: string): Promise<ResponseData> {
+        await console.log(inspectionNumber)
+        return { code: 200, data: { hi: "Hello world from getInspectionDetails" } }
+    }
 }
 
-export async function getInspectionDetails(inspectionNumber: string): Promise<ResponseData> {
-    await console.log(inspectionNumber)
-    return { code: 200, data: { hi: "Hello world from getInspectionDetails" } }
-}
