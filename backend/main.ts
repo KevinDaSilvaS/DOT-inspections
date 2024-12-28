93395363831421500d765c11f0f0a3fcf4710687
Kevin <kevin.kds80@gmail.com>
2024-12-28 11:41:41 -0300

    add db connection on startup

--- backend/main.ts
+++ backend/main.ts
@@ -1,9 +1,9 @@
 import { Application, Router } from "@oak/oak";
-import {
-  fetchInspectionDetails,
-  fetchInspections,
-} from "./controllers/inspections.ts";
+import { InspectionsController } from "./controllers/inspections.ts";
+import { connect } from "./repositories/start_db.ts";
 
+const db = await connect();
+const inspectionsController = new InspectionsController(db);
 const router = new Router();
 
 router.get("/inspections", async (ctx) => {
@@ -13,7 +13,7 @@ router.get("/inspections", async (ctx) => {
   const filter = url.searchParams.get("filter") ?? "";
   const page = url.searchParams.get("page") ?? "1";
   const limit = url.searchParams.get("limit") ?? "10";
-  const { code, data } = await fetchInspections(
+  const { code, data } = await inspectionsController.fetchInspections(
     parseInt(page),
     parseInt(limit),
     sort,
@@ -26,7 +26,7 @@ router.get("/inspections", async (ctx) => {
 });
 
 router.get("/inspections/:inspection_number", async (ctx) => {
-  const { code, data } = await fetchInspectionDetails(
+  const { code, data } = await inspectionsController.fetchInspectionDetails(
     ctx.params.inspection_number,
   );
   ctx.response.headers.append("Content-Type", "application/json");
