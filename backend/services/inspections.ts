b788d1dab493e49f8a1bf56555b86636611daeaa
Kevin <kevin.kds80@gmail.com>
2024-12-28 12:21:50 -0300

    move service to class

--- backend/services/inspections.ts
+++ backend/services/inspections.ts
@@ -5,22 +5,54 @@ import { inspections } from "../repositories/inpections/client.ts";
 import { InspectionSchema } from "../repositories/inpections/schema.ts";
 
 export class InspectionsService {
-    private db: Database;
-    private inspections: Collection<InspectionSchema>;
+  private db: Database;
+  private inspections: Collection<InspectionSchema>;
 
-    constructor(db: Database) {
-        this.db = db;
-        this.inspections = inspections(this.db);
-    }
+  constructor(db: Database) {
+    this.db = db;
+    this.inspections = inspections(this.db);
+  }
 
-    public async getInspections(params: InspectionsSelector): Promise<ResponseData> {
-        await console.log(params)
-        return { code: 201, data: { hi: "Hello world from getInspections" } }
+  public async getInspections(
+    params: InspectionsSelector,
+  ): Promise<ResponseData> {
+    const { page, limit, filter, field, sort } = params
+    const sortObj: Record<string, number> = {}
+    sortObj[field] = sort;
+
+    const filterObj: Record<string, string | number | boolean> = {}
+    if(filter) {
+        filterObj["basic"] = filter;
     }
     
-    public async getInspectionDetails(inspectionNumber: string): Promise<ResponseData> {
-        await console.log(inspectionNumber)
-        return { code: 200, data: { hi: "Hello world from getInspectionDetails" } }
-    }
-}
+    const inspections = await this.inspections.find(filterObj, {
+      projection: {
+        _id: false,
+        report_number: true,
+        status: true,
+        date: true,
+        weight: true,
+        basic: true,
+        license_plate: true,
+      },
+    })
+    .sort(sortObj)
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .toArray();
+    return { code: 201, data: inspections };
+  }
 
+  public async getInspectionDetails(
+    inspectionNumber: string,
+  ): Promise<ResponseData> {
+    const inspection = await this.inspections.findOne({
+      report_number: inspectionNumber,
+    });
+    if (!inspection) {
+      return { code: 404, data: { error: "Inspection not found :(" } };
+    }
+
+    return { code: 200, data: inspection };
+  }
+}
